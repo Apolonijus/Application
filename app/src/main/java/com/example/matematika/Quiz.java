c2ddafd16bb8d1974f10ad06a9ff61ee8d1bf3fe
Verbuotojas <bombonkes666>
2020-03-17 21:17:19 +0200

    test?

--- app/src/main/java/com/example/matematika/Quiz.java
+++ app/src/main/java/com/example/matematika/Quiz.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Quiz extends AppCompatActivity {
     private Button button;
-
+    int x;
 
 
 
@@ -18,15 +21,29 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.button_back);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 backActivity();
             }
+
+
         });
 
+        button = (Button) findViewById(R.id.button_ans);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int number = rand.nextInt(10);
+                x=number;
+                TextView myText = (TextView)findViewById(R.id.textView2);
+                String myString = String.valueOf(number);
+                myText.setText(myString);
 
+            }
+        });
 
 
 
