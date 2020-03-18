c46c9afa2eb388162b148f7309f2a389d3ef2939
Apolonijus <rokasbasinskas@gmail.com>
2020-03-18 18:56:28 +0200

    Blank

--- app/src/main/java/com/example/matematika/Quiz.java
+++ app/src/main/java/com/example/matematika/Quiz.java
@@ -16,6 +16,7 @@ public class Quiz extends AppCompatActivity {
     private Button button;
     int x;
     int y;
+    int z;
     private EditText editText;
     private Button checkbutton;
 
@@ -44,22 +45,51 @@ public class Quiz extends AppCompatActivity {
             public void onClick(View view) {
                 Random rand = new Random();
                 Random rand2 = new Random();
+                Random rand3 = new Random();
 
                 int number = rand.nextInt(10);
                 int number2 = rand2.nextInt(25);
+                int number3 = rand3.nextInt(4);
 
                 x=number;
                 y=number2;
+                z=number3;
 
                 TextView myText = (TextView)findViewById(R.id.textView2);
                 TextView myText2 = (TextView) findViewById(R.id.textView3);
 
 
+
+
                 String myString = String.valueOf(number);
                 String myString2 = String.valueOf(number2);
 
+
                 myText.setText(myString);
                 myText2.setText(myString2);
+
+                if (z==0)
+                {
+                    TextView tx5_1 = findViewById(R.id.textView);
+                    tx5_1.setText("+");
+
+                }
+                else if (z==1)
+                {
+                    TextView tx5_2 = findViewById(R.id.textView);
+                    tx5_2.setText("-");
+                }
+                else if (z==2)
+                {
+                    TextView tx5_2 = findViewById(R.id.textView);
+                    tx5_2.setText("*");
+                }
+                else
+                {
+                    TextView tx5_2 = findViewById(R.id.textView);
+                    tx5_2.setText("/");
+                }
+
 
             }
         });
@@ -81,7 +111,7 @@ public class Quiz extends AppCompatActivity {
 
                 TextView tx3 = (TextView) findViewById(R.id.textView2);
                 int amount_x = Integer.parseInt(tx3.getText().toString());
-                int sum = amount+amount_x+amount_y;
+               // int sum = amount+amount_x+amount_y;
 
                     // TextView myText4 =   (TextView) findViewById(R.id.textView4);
                      //String myString4 = String.valueOf(sum);
@@ -91,18 +121,64 @@ public class Quiz extends AppCompatActivity {
                // tx4.setText("Sveikas");
 
 
+                if (z==0)
+                {
+                    if ((amount_x + amount_y) == amount )
+                    {
+                        TextView tx4 = (TextView) findViewById(R.id.textView4);
+                        tx4.setText("Atsakymas yra teisingas");
+                    }
 
-
-                if ((amount_x + amount_y) == amount ) {
-                    TextView tx4 = (TextView) findViewById(R.id.textView4);
-                    tx4.setText("Atsakymas yra teisingas");
+                    else
+                    {
+                        TextView tx4_1 = (TextView) findViewById(R.id.textView4);
+                        tx4_1.setText("Deja, atsakymas nera teisingas");
+                    }
                 }
+                else if (z==1)
+                {
+                    if ((amount_x - amount_y) == amount )
+                    {
+                        TextView tx4 = (TextView) findViewById(R.id.textView4);
+                        tx4.setText("Atsakymas yra teisingas");
+                    }
 
+                    else
+                    {
+                        TextView tx4_1 = (TextView) findViewById(R.id.textView4);
+                        tx4_1.setText("Deja, atsakymas nera teisingas");
+                    }
+                }
+                else if (z==2)
+                {
+                    if ((amount_x * amount_y) == amount )
+                    {
+                        TextView tx4 = (TextView) findViewById(R.id.textView4);
+                        tx4.setText("Atsakymas yra teisingas");
+                    }
+
+                    else
+                    {
+                        TextView tx4_1 = (TextView) findViewById(R.id.textView4);
+                        tx4_1.setText("Deja, atsakymas nera teisingas");
+                    }
+                }
                 else
                 {
-                    TextView tx4_1 = (TextView) findViewById(R.id.textView4);
-                    tx4_1.setText("Deja, atsakymas nera teisingas");
+                    if ((amount_x / amount_y) == amount )
+                    {
+                        TextView tx4 = (TextView) findViewById(R.id.textView4);
+                        tx4.setText("Atsakymas yra teisingas");
+                    }
+
+                    else
+                    {
+                        TextView tx4_1 = (TextView) findViewById(R.id.textView4);
+                        tx4_1.setText("Deja, atsakymas nera teisingas");
+                    }
                 }
+
+
 
             }
         });
