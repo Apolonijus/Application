da7dc88389cb9f29575aa3db714c2ce2f3c5302a
Apolonijus <rokasbasinskas@gmail.com>
2020-03-18 16:42:01 +0200

    Blank

--- app/src/main/java/com/example/matematika/Quiz.java
+++ app/src/main/java/com/example/matematika/Quiz.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,6 +15,9 @@ import java.util.Random;
 public class Quiz extends AppCompatActivity {
     private Button button;
     int x;
+    int y;
+    //private EditText editText;
+   // private Button checkbutton;
 
 
 
@@ -32,18 +36,35 @@ public class Quiz extends AppCompatActivity {
         });
 
         button = (Button) findViewById(R.id.button_ans);
+       // editText= (EditText) findViewById(R.id.editText);
+       // checkbutton= (Button) findViewById(R.id.button_check);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Random rand = new Random();
+                Random rand2 = new Random();
+
                 int number = rand.nextInt(10);
+                int number2 = rand2.nextInt(25);
+
                 x=number;
+                y=number2;
+
                 TextView myText = (TextView)findViewById(R.id.textView2);
+                TextView myText2 = (TextView) findViewById(R.id.textView3);
+
                 String myString = String.valueOf(number);
+                String myString2 = String.valueOf(number2);
+
                 myText.setText(myString);
+                myText2.setText(myString2);
 
             }
         });
+
+
+
 
 
 
